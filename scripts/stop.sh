e7ccf42639ddf1d0fe87475923a5da181f7914c4
range31 <ltw20140917@gmail.com>
2021-04-01 23:38:56 +0900

    profile 배포

---
+++ scripts/stop.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT 에서 구동 중인 PID"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동 중인 PID 없습니다"
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PORT}
+  sleep 5
+fi
