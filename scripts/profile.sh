68e2433c344f6b20fec8fb8f6961ebe5c82707be
range31 <ltw20140917@gmail.com>
2021-04-02 20:11:30 +0900

    profile 배포

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -24,7 +24,7 @@ function find_idle_profile() {
 }
 
 #쉬고 있는 profile port 찾기
-function find_idle_prot() {
+function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
 
     if[ ${IDLE_PROFILE} == real1 ]
